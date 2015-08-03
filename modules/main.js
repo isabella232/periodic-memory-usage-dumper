2a0ead5a3cad3e876d67a92c7c80fef2bef17bf2
YUKI Hiroshi <shimoda@clear-code.com>
2015-08-03 18:22:37 +0900

    Define features as a service object

--- modules/main.js
+++ modules/main.js
@@ -1,9 +1,10 @@
 var BASE = 'extensions.periodic-memory-usage-dumper@piro.sakura.ne.jp.';
-
 var prefs = require('lib/prefs').prefs;
-prefs.setDefaultPref(BASE + 'anonymize', false);
-prefs.setDefaultPref(BASE + 'intervalSeconds', 60);
+
 {
+  prefs.setDefaultPref(BASE + 'anonymize', false);
+  prefs.setDefaultPref(BASE + 'intervalSeconds', 60);
+
   let dir = Cc['@mozilla.org/file/directory_service;1']
                .getService(Components.interfaces.nsIProperties)
                .get('Home', Components.interfaces.nsIFile);
@@ -14,28 +15,29 @@ prefs.setDefaultPref(BASE + 'intervalSeconds', 60);
 var timer = Cu.import('resource://gre/modules/Timer.jsm');
 var { Promise } = Cu.import('resource://gre/modules/Promise.jsm');
 
-function generateDumpFilename() {
+var periodicDumper = {
+  generateDumpFilename: function() {
   var now = new Date();
   var timestamp = now.toISOString().replace(/:/g, '-');
   return timestamp + '.json.gz';
-}
+  },
 
-function prepareDirectory(aDir) {
+  prepareDirectory: function(aDir) {
   if (aDir.parent)
-    prepareDirectory(aDir.parent);
+    this.prepareDirectory(aDir.parent);
   if (!aDir.exists())
     aDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0700);
-}
+  },
 
-function dumpMemoryUsage() {
+  dumpMemoryUsage: function() {
   console.log('dump start');
-  return new Promise(function(resolve, reject) {
-    var localName = generateDumpFilename();
+  return new Promise((function(resolve, reject) {
+    var localName = this.generateDumpFilename();
     var file = Cc['@mozilla.org/file/local;1']
                  .createInstance(Ci.nsILocalFile);
     file.initWithPath(prefs.getPref(BASE + 'outputDirectory'));
     file.append(localName);
-    prepareDirectory(file.parent);
+    this.prepareDirectory(file.parent);
 
     var anonymize = prefs.getPref(BASE + 'anonymize');
     var start = Date.now();
@@ -45,29 +47,30 @@ function dumpMemoryUsage() {
       console.log('dump finish (' + ((Date.now() - start) / 1000) + 'sec.)');
       resolve();
     }, null, anonymize);
-  });
-}
+  }).bind(this));
+  },
 
-var lastTimeout;
+  lastTimeout: null,
 
-function onTimeout() {
-  dumpMemoryUsage()
-    .then(function() {
+  onTimeout: function() {
+    this.dumpMemoryUsage()
+    .then((function() {
       var interval = Math.max(1, prefs.getPref(BASE + 'intervalSeconds'));
-      lastTimeout = timer.setTimeout(onTimeout, interval * 1000);
-    })
+      lastTimeout = timer.setTimeout(this.onTimeout.bind(this), interval * 1000);
+    }).bind(this))
     .catch(function(error) {
       Cu.reportError(error);
     });
-}
+  }
+};
 
-onTimeout();
+periodicDumper.onTimeout();
 
 function shutdown() {
-  if (lastTimeout)
-    timer.clearTimeout(lastTimeout);
+  if (periodicDumper.lastTimeout)
+    timer.clearTimeout(periodicDumper.lastTimeout);
 
   timer = Promise = prefs =
-    dumpMemoryUsage = lastTimeout = onTimeout =
+    periodicDumper =
       undefined;
 }
