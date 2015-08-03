2e725deb3df9849491e998bf5dc6569dfd167ff0
YUKI Hiroshi <shimoda@clear-code.com>
2015-08-03 18:40:55 +0900

    Enlarge minimum idle seconds from 1 to 10

--- modules/main.js
+++ modules/main.js
@@ -92,7 +92,7 @@ var periodicDumper = {
 
 var idleService = Cc['@mozilla.org/widget/idleservice;1']
                     .getService(Ci.nsIIdleService);
-var idleSeconds = Math.max(1, prefs.getPref(BASE + 'idleSeconds'));
+var idleSeconds = Math.max(10, prefs.getPref(BASE + 'idleSeconds'));
 idleService.addIdleObserver(periodicDumper, idleSeconds);
 
 function shutdown() {
